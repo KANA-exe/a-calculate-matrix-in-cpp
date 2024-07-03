0ef50e6da1e440aadba246aa821df534c3b2f2ff
KANA <kana960209@gmail.com>
2024-07-04 03:58:17 +0800

    modify edit.cpp

--- edit.cpp
+++ edit.cpp
@@ -1,63 +1,158 @@
 #include<iostream>
+#include<string>
 #include"matrix_cal.h"
 
 using namespace std;
 
-void inv_mtx(double** a,int i,int j,double** ans){
-    if(i!=j||i!=2||i!=2){
-        cout << "error:the matrix cannot calculate second order determainate";
-        exit;
-    }
-
-    double s[2][2]={{a[1][1],a[0][1]},{a[1][0],a[0][0]}};
-    double c=1/det_mtx(a,i,j);
-    double h=0;
-    s[0][1]=s[0][1]*-1;
-    s[1][0]=s[1][0]*-1;
-
-    for(int d=0;d<2;d++){
-        for(int l=0;l<2;l++){
-            
-        }
-    }
-}
-
 main()
 {
-    //import journey 
-    int i=2;
-    int j=3;
-
-    //create matrix a and pointer ad_a
-    double a[i][j]={{3,1},{1,0}};
+    //import basic information of matrix
+    int qua;//qua:quantity of matrix
     
-    double* ad_a[i]={};
-    for(int t=0;t<i;t++){
-        ad_a[t]=a[t];
+    cout <<"input the matrix quantity.(input a number)";//input quantity of matrix
+    cin >>qua;
+
+    int li;
+    int lj; 
+    cout <<"input the largest matrix.(if it's a*b,input a b.)";//input largest matrix of size of matrix
+    cin >>li >>lj;
+
+    //declare matrix of array and matrix index of array
+    double mtx[qua][li][lj]={};
+    int i[qua]={};
+    int j[qua]={};
+    string ch[qua]={};//name of matrix
+
+    //input matrix
+    for(int in=0;in<qua;in++){
+        cout <<"input the martix name.";//input matrix name
+        cin >> ch[in];
+
+        cout <<"input the lines and row.";//input line and row
+        cin >> j[in] >>i[in];
+
+        for(int s=0;s<i[in];s++){//input the metrix
+            cout <<"input the " <<s <<"-th row.(extra input will be ignored)";
+            for(int x=0;x<j[in];x++){
+                cin >>mtx[in][s][x];
+            }
+        }
     }
 
-    //ceate matrix c and pointer ad_c
-    double c[j][i]={{2,-1},{3,2},{5,3}};
-    
-    double* ad_c[j]={};
-    for(int t=0;t<j;t++){
-        ad_c[t]=c[t];
+    bool d=true;
+    while(d){//check and modify
+        cout <<"please check all matrix." <<endl;//output all of matrix
+        for(int s=0;s<qua;s++){
+                //create temporary pointer
+                double* tem[i[s]]={};
+                for(int a=0;a<i[s];a++){
+                    tem[a]=mtx[s][a];
+                }
+
+                //output matrix
+                cout <<ch[s] <<"=" <<endl;
+                expt_mtx(tem,i[s],j[s]);
+                cout <<endl;
+
+                //free memory
+                for(int t;t<i[s];t++){
+                    delete [] tem[t];
+                }
+            }
+        
+        
+        
+        string ans;
+
+        //check matrix
+        cout <<"Do these matrices contain errors?(input Yes/No)";
+        cin >>ans;
+
+        //determine whether modification is needed
+        if(ans=="No"){//not need
+            string a;
+            cout <<"sure?(input Yes/No)(cannot be modified later)";//last check no to modify
+            if(a=="Yes"){
+                d=false;
+                break;
+            }
+            else if(a=="No"){
+
+            }
+            else{//invalid input
+                cout <<"error:the input is invalid"
+            }
+        }
+        else if(ans=="Yes"){//need
+                
+            bool o=true;
+            while(o){
+                    
+                string answ;
+                cout <<"input name of matrix that need to modify(if don't need,type /end)";//input name of matrix that need to modify
+                cin >>answ;
+                
+                if(answ=="/end"){//determine whether it is over
+                    o=false;
+                        break;
+                }
+                else{
+                    int se;
+                    bool p=true;
+                    while(p){
+                        bool n=true;
+
+                        for(int ser=0;ser<qua;ser++){//search matrix
+                            if(ch[ser]==answ){//found
+                                se=ser;
+                            }
+                            else{//not found
+                                cout <<"error:not found the matrix";
+                                n=false;
+                            }
+                        }
+                        while (n)
+                        {
+                            string an;
+                            cout <<"input element you want to modify(input name/element.if don't need,type /end)";
+                            cin >>an;
+                            if(an=="/end"){//end modify
+                                n=false;
+                                p=false;
+                                break;
+                            }
+                            else if(an=="name"){//modify name
+                            string naw;
+                            cout <<"input new name";
+                            cin >>naw;
+                            }
+                            else if (an=="element"){//modify element
+                                int ans;
+                                cout <<"input the row that need to modify";
+                                cin >>ans;
+
+                                double ac;
+                                cout <<"input the new row";
+
+                                for(int t;t<i[se];t++){
+                                    cin >>ac;
+                                    mtx[se][ans][t]=ac;
+                                }
+                            }
+                            else{//invalid input
+                                cout <<"error:the input is invalid";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else{//invalid input
+            cout <<"error:input is invalid";
+        }
     }
-
-    double sf=10;
     
-    //ceate a matrix for answer
-    double* ans[i]={};
-
-    //calculate and export
-    inv_mtx(ad_a,i,i,ans);
-    expt_mtx(ans,i,i);
-
-    // free dynamically allocated memory
-    for (int t=0;t<i;t++) {
-        delete[] ans[t];
-    }
-    
+    cout <<"**********input finish***********";
     
     return 0;
 }
